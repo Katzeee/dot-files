ce03ab235ea32712076067b1fe224139b230ad09
xac <jiangfengxi.c@gmail.com>
2022-10-02 19:55:18 +0800

    nvim cmp setup

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,103 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Eazy line navigation
+keymap("", "<S-h>", "0", opts)
+keymap("", "<S-l>", "$", opts)
+
+-- Navigate buffers
+-- keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- Indent
+keymap("n", "<", "v<gv<Esc>", opts)
+keymap("n", ">", "v>gv<Esc>", opts)
+
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Eazy line navigation
+-- keymap("v", "<S-h>", "0", opts)
+-- keymap("v", "<S-l>", "$", opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Eazy line navigation
+keymap("x", "<S-h>", "0", opts)
+keymap("x", "<S-l>", "$", opts)
+
+-- Operation --
+-- Eazy line navigation
+-- keymap("o", "<S-h>", "0", opts)
+-- keymap("o", "<S-l>", "$", opts)
+
+-- Command --
+-- Cmdline Completion
+vim.keymap.set("c", "<Tab>", function()
+  return vim.fn.pumvisible() == 1 and "<Space>" or "<C-Z>" 
+end, {expr = true, noremap = true})
+vim.keymap.set("c", "<C-k>", function()
+  return "<C-P>"
+end, {expr = true})
+vim.keymap.set("c", "<C-j>", function()
+  return "<C-N>"
+end, {expr = true})
+
+-- Terminal --
+-- Better terminal navigation
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
