90adc539eb86ac8943c3bb6bae5ec37b370f619e
xac <jiangfengxi.c@gmail.com>
2023-01-17 00:51:59 +0800

    zsh config

--- pre-config.sh
+++ pre-config.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "\n[archlinuxcn]\nServer = https://mirrors.ustc.edu.cn/archlinuxcn/$arch\n"
+set -e
+set -o pipefail
+echo -e "\n[archlinuxcn]\nServer = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch" >> /etc/pacman.conf
 pacman -Syy
-pacman -S archlinuxcn-keyring --noconfirm
-pacman -S yay --noconfirm
+pacman -S archlinuxcn-keyring --noconfirm --needed
+pacman -S yay --noconfirm --needed
 sed -i '/^#en_US/s/#//'  /etc/locale
 sed -i '/^#en_US/s/#//'  /etc/locale
 locale-gen
-pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji adobe-source-han-sans-otc-fonts wqy-microhei wqy-zenhei --noconfirm
+pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji adobe-source-han-sans-otc-fonts wqy-microhei wqy-zenhei --noconfirm --needed
--- zsh/zsh-config.sh
+++ zsh/zsh-config.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+set -e
+set -o pipefail
+if [ `whoami` == "root" ]; then echo "no root"; exit; fi
+yay -S zsh --noconfirm --needed
+curl -fsSL https://gitee.com/mirrors/oh-my-zsh/raw/master/tools/install.sh | sh
+cp .zshrc ~/.zshrc
+chsh -s /usr/bin/zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
