e501a214c7900e29cadc3e6adb89d3ae7eec608b
xac <jiangfengxi.c@gmail.com>
2023-12-27 11:23:10 +0800

    [zsh] use p10k theme

--- zsh/zsh-config.sh
+++ zsh/zsh-config.sh
@@ -27,6 +27,7 @@ fi
 git clone https://github.com/zsh-users/zsh-autosuggestions ${plugin_path}/zsh-autosuggestions
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${plugin_path}/fast-syntax-highlighting
 git clone https://github.com/jeffreytse/zsh-vi-mode ${plugin_path}/zsh-vi-mode
+git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ${plugin_path}/.z
 
 source ~/.zshrc
