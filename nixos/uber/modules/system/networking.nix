ceaf58be1e88a038c61f1c03c50032fbcd10cf7b
xac <jiangfengxi.c@gmail.com>
2026-02-23 13:12:44 +0800

    [nixos] configure NetworkManager and systemd-resolved with custom DNS settings

--- nixos/uber/modules/system/networking.nix
+++ nixos/uber/modules/system/networking.nix
@@ -2,4 +2,13 @@
 
 {
   networking.networkmanager.enable = true;
+  networking.networkmanager.dns = "systemd-resolved";
+
+  services.resolved.enable = true;
+  services.resolved.settings = {
+    Resolve = {
+      DNS = [ "223.5.5.5" "223.6.6.6" ];
+      FallbackDNS = [ "114.114.114.114" "1.1.1.1" "8.8.8.8" ];
+    };
+  };
 }
